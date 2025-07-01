09f0c30040229778a601627fc8e03f1795e68d41
Işıl <isilulgerr@gmail.com>
2025-07-02 02:05:24 +0300

    fix: remove admin_service. prefix

--- admin_service/routes/admin_routes.py
+++ admin_service/routes/admin_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required
-from admin_service.services.admin_service import add_room_to_db, update_room_in_db
+from services.admin_service import add_room_to_db, update_room_in_db
 
 admin_bp = Blueprint("admin", __name__)
 
