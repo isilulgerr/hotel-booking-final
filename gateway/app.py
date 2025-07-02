aaeef38264f27318c2619cabea1c8d592a320a8e
Işıl <isilulgerr@gmail.com>
2025-07-02 18:37:11 +0300

    fix: service urls

--- gateway/app.py
+++ gateway/app.py
@@ -9,7 +9,7 @@ CORS(app)
 SERVICE_MAP = {
     "admin": "https://admin-service-8014.onrender.com",
     "agent": "https://agent-service-v59b.onrender.com",
-    "booking": "https://book-service-9dtv.onrender.com",
+    "book": "https://book-service-9dtv.onrender.com",
     "comments": "https://comments-service-o4l5.onrender.com",
     "hotel": "https://hotel-service-zmjn.onrender.com",
     "notification": "https://notification-service-ig42.onrender.com",
